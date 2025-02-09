74a3090c25de3551dd17fcf59de742394b52533f
Andy Chan <andy.chan@ascenda.com>
2025-02-09 11:45:21 +0800

    Convert page into a client component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
+"use client";
+
 import TaxCalculator from "@/components/tax-calculator";
+import { useIsClient } from "@uidotdev/usehooks";
 
 export default function Home() {
+  const isClient = useIsClient();
+
+  if (isClient === false) {
+    return null;
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="mb-8 text-4xl font-bold">Singapore Tax Savings Viewer</h1>
